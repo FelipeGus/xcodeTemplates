7e1dc8b9bc59809c733aa9fcd297f46694ebc01b
Felipe Figueiredo <felipegusfigueiredo@gmail.com>
2018-07-07 16:05:10 -0300

    Initial commit

---
+++ VMVC/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,49 @@
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    
+    var viewModel: ___VARIABLE_productName___ViewModel!
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    // MARK: - Init
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    // MARK: - LifeCycle
+    
+    func viewDidLoad() {
+        super.viewDidLoad()
+        setupInitialState()
+    }
+    
+    // MARK: - View Configuration Methods
+
+    func setupInitialState() {
+        bindViewModel()
+    }
+}
+
+// MARK: - VC -- VM Binding
+
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    func bindViewModel() {
+        viewModel.state.asDriver()
+            .drive( onNext: { [weak self] (newState) in
+                switch newState {
+                default:
+                    break
+                }
+            }).disposed(by: disposeBag)
+    }
+}
---
+++ VMVC/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,17 @@
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+
+    var state: BehaviorRelay<State> = BehaviorRelay(value: .begin)
+    var dataManager: ___VARIABLE_MODULENAME___DataManager
+
+    init(dataManager: ___VARIABLE_MODULENAME___DataManager) {
+        self.dataManager = dataManager
+    }
+    
+}
