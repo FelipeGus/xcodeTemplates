201127ab00321efa2cef9898b0290d9aa672a818
Felipe Figueiredo <felipegusfigueiredo@gmail.com>
2018-08-11 17:50:41 -0300

    Adding info plist to VP template. Addin new Coordinator Template.

---
+++ Coordinator/___FILEBASENAME___Coordinator.swift
@@ -0,0 +1,31 @@
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+
+import Foundation
+import UIKit
+
+// MARK: - Presenter Input
+
+final class ___FILEBASENAMEASIDENTIFIER___Coordinator: Coordinator {
+    
+    // MARK: - Properties
+    var childCoordinators: [Coordinator] = []
+    var services: Services
+    
+    lazy var rootViewController: UINavigationController = {
+        let navigationController = UINavigationController()
+        navigationController.isNavigationBarHidden = true
+        return navigationController
+    }()
+    
+    // MARK: - Init
+    
+    init(services: Services) {
+        self.services = services
+    }
+    
+    /// Starts the coordinator
+    func start() {
+    }
+    
+}
